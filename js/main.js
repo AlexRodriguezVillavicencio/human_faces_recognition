dcb7568bc6486910d7b47d8908ad3ea6505bccb7
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 <arodriguezv@uni.pe>
2023-06-11 21:43:58 -0500

    demo 4

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 const containtText = document.getElementById("containtText");
 let parsedText;
 
+async function load_model(img){
+    button_detection.disabled = true;
+    const imageTensor = tf.browser.fromPixels(img,1).expandDims();;
+    const prediction = model.predict(imageTensor);
+    const predictionArray = await prediction.array();
+    tf.dispose([imageTensor, prediction]);
+    model.dispose();
+    return predictionArray
+}
+
 button_detection.addEventListener("click", async function() {
   var htmlContent = `
   <div class="text__group">
@@ -11,11 +21,10 @@ button_detection.addEventListener("click", async function() {
   </div>
     `;
   containtText.innerHTML = htmlContent
-  console.log('---')
   const insertText = document.getElementById("insertText");
     const imageData = context.getImageData(0, 0, 300,300);
     const namePre = await load_model(imageData)
-    console.log(parseFloat(namePre).toFixed(3))
+    button_detection.disabled = false;
     if (namePre >= 0.5){
       insertText.innerHTML = `Human:  ${parseFloat(namePre).toFixed(3)*100}%`;
     }else {
